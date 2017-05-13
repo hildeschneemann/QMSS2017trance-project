d99349b2ce1553726072f2aa5b12abf5ba0572dc
Hilde <Hilde>
2017-05-13 17:39:06 +0200

    code update

--- analysis/hildesSecretCode.R
+++ analysis/hildesSecretCode.R
@@ -70,3 +70,59 @@ mean(lambda)
 posterior.mode(lambda)
 HPDinterval(lambda)
 
+prior.bantu3<-list(G=list(G1=list(V=1,nu=0.002)),R=list(V=1,fix=1))
+
+model3<-MCMCglmm(trance_binary~Variable..Precipitation.Constancy...,
+                 random=~treelabel, 
+                 ginverse=list(treelabel=inverseA(finaltree)$Ainv), 
+                 prior = prior.bantu3, 
+                 verbose=F, #this will let us see how fast the model is running
+                 family="categorical",
+                 data = bantudata,
+                 nitt=1000000, #number of iterations (this is a very small number! a real analysis would run for much longer)
+                 thin=500, #sampling of iterations (will record every 50th iteration, for a total posterior sample size of 1000)
+                 burnin=100000) #initial iterations to discard
+
+summary(model3)
+plot(model3)
+
+lambda<-model3$VCV[,"treelabel"]/(model3$VCV[,"treelabel"]+model3$VCV[,"units"])
+
+mean(lambda)
+posterior.mode(lambda)
+HPDinterval(lambda)
+
+
+
+install.packages("ggtree")
+library(ggtree)
+
+
+
+prior.bantuChi<-list(G=list(G1=list(V=1,nu=1000, alpha.mu=0, alpha.V=1)),R=list(V=1,fix=1))
+
+modelChi<-MCMCglmm(trance_binary~Variable..Precipitation.Constancy...,
+                 random=~treelabel, 
+                 ginverse=list(treelabel=inverseA(finaltree)$Ainv), 
+                 prior = prior.bantuChi, 
+                 verbose=F, #this will let us see how fast the model is running
+                 family="categorical",
+                 data = bantudata,
+                 nitt=10000000, #number of iterations (this is a very small number! a real analysis would run for much longer)
+                 thin=1000, #sampling of iterations (will record every 50th iteration, for a total posterior sample size of 1000)
+                 burnin=1000000) #initial iterations to discard
+
+summary(modelChi)
+plot(modelChi)
+
+lambda<-modelChi$VCV[,"treelabel"]/(modelChi$VCV[,"treelabel"]+modelChi$VCV[,"units"])
+
+mean(lambda)
+posterior.mode(lambda)
+HPDinterval(lambda)
+
+
+#plot traits on tree
+plot(finaltree,show.tip.label=FALSE)
+tiplabels(pie=bantudata$trance_binary, cex=0.4)
+#add.simmap.legend(colors=cols) #click where you want to draw your legend!
